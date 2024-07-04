45719b9b2a714f36ac9285dddb0d731ffbb9b028
Seung Ki Lee <seungkilee.cs@gmail.com>
2024-07-04 17:17:56 +0900

    autodeploy-2024-07-04_17_17_55

--- git-branch-deploy.sh
+++ git-branch-deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Pushing directly to master for dist deployment
-
 # Check if the current directory is a git repository
 if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
     echo "This is a git repository. The script will run."
@@ -31,8 +29,29 @@ if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
     # Commit the changes
     git commit -m "$commit_message"
 
+    # Create the branch name
+    branch_name="autodeploy-$timestamp"
+
+    # Create a new branch and switch to it
+    git checkout -b $branch_name
+
+    # Push the new branch to the remote repository
+    git push origin $branch_name
+
+    # Switch back to the master branch
+    git checkout master
+
+    # Merge the new branch into master
+    git merge $branch_name
+
     # Push the changes to the master branch at the remote repository
     git push origin master
+
+    # Delete the local branch
+    git branch -d $branch_name
+
+    # Delete the branch from the remote repository
+    git push origin --delete $branch_name
 
 else
     echo "This is not a git repository. The script will not run."
